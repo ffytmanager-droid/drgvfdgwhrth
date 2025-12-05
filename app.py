56e50685ac7a53be44ffb178b09949593a9ef16b
ffytmanager-droid <ffytmanager@gmail.com>
2025-12-06 00:39:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -922,6 +922,7 @@ def main():
     
     # Add error handler
     application.add_error_handler(error_handler)
+    application.run_webhook(listen="0.0.0.0", port=5000)
     
     # Start the Bot
     print("Bot is running...")
